d9fd912b0643deddaece62a714e81ed2d2876f49
hereticpurge <hereticpurge@gmail.com>
2019-02-05 10:47:37 -0700

    Continuing to learn and setup vectors.

--- vector.js
+++ vector.js
@@ -2,6 +2,14 @@ var vector = {
     _x: 1,
     _y: 0,
 
+    create: function(x, y) {
+        var obj = Object.create(this);
+
+        obj.setX(x);
+        obj.setY(y);
+        return obj;
+    },
+
     setX: function (value) {
         this._x = value;
     },
